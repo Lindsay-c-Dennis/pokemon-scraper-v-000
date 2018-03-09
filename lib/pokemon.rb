b8bb3c62950733a0239fc6eb5f7b32a8769d6768
Lindsay Dennis <lindsay.c.dennis@gmail.com>
2018-03-09 18:11:32 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -16,9 +16,6 @@ class Pokemon
     db.execute("INSERT INTO pokemon (name, type) VALUES (?,?)", name, type)
   end
   
-  def self.all 
-    @@all 
-  end
   
   def self.find(id, db)
     pokemon_info = db.execute("SELECT * FROM pokemon WHERE id == ?", id).flatten
